d76ee3773089beb819686fd8ac76ca197dac061c
unknown <luolang304019@outlook.com>
2018-08-03 20:46:09 +0800

    LeetCodes

---
+++ TwoSum1.py
@@ -0,0 +1,16 @@
+'''
+给定 nums = [2, 7, 11, 15], target = 9
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+'''
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return i, j
